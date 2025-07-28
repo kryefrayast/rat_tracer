d2abeb806ad7f8cb6807d650698934bfad8d92ed
kryefrayast <24johnszc@sjtu.edu.cn>
2025-07-28 20:46:45 +0800

    Updated commit

--- ray.rs
+++ ray.rs
@@ -4,17 +4,24 @@ use crate::vec3::{Point3, Vec3};
 pub struct Ray {
     orig: Point3,
     dir: Vec3,
+    tm: f64,
 }
 
 impl Ray {
     pub fn new(origin: Point3, direction: Vec3) -> Self {
-        Ray {orig: origin, dir: direction}
+        Ray {orig: origin, dir: direction, tm: 0.0}
+    }
+    pub fn with_time(origin: Point3, direction: Vec3, time: f64) -> Self {
+        Ray {orig: origin, dir: direction, tm: time}
     }
     pub fn origin(&self) -> Point3 {
         self.orig
     }
     pub fn direction(&self) -> Vec3 {
         self.dir
+    }
+    pub fn time(&self) -> f64 {
+        self.tm
     }
     pub fn at(&self, t: f64) -> Point3 {
         self.orig + t * self.dir
@@ -26,6 +33,8 @@ impl Default for Ray {
         Ray {
             orig: Point3::default(), 
             dir: Vec3::default(),
+            tm: 0.0
         }
     }
 }
+
