0e4f0e67ffa1750bbe2cd7b8f6bb211aa61e84ef
kryefrayast <24johnszc@sjtu.edu.cn>
2025-07-28 20:43:44 +0800

    Updated commit

--- interval.rs
+++ interval.rs
@@ -1,4 +1,5 @@
 use std::f64::{INFINITY, NEG_INFINITY};
+use std::ops::Add;
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Interval {
@@ -43,6 +44,18 @@ impl Interval {
         }
     }
     
+    pub fn expand(&self, delta: f64) -> Self {
+        let padding = delta / 2.0;
+        Self::with_bounds(self.min - padding, self.max + padding)
+    }
+    
+    pub fn merge(&self, other: &Self) -> Self {
+        Self {
+            min: self.min.min(other.min),
+            max: self.max.max(other.max),
+        }
+    }
+    
     pub const EMPTY: Self = Self {
         min: INFINITY,
         max: NEG_INFINITY,
@@ -52,10 +65,33 @@ impl Interval {
         min: NEG_INFINITY,
         max: INFINITY,
     };
+    
+    pub fn with_displacement(&self, displacement: f64) -> Self {
+        Self {
+            min: self.min + displacement,
+            max: self.max + displacement,
+        }
+    }
 }
 
 impl Default for Interval {
     fn default() -> Self {
         Self::new()
+    }
+}
+
+impl Add<f64> for Interval {
+    type Output = Self;
+    
+    fn add(self, displacement: f64) -> Self::Output {
+        self.with_displacement(displacement)
+    }
+}
+
+impl Add<Interval> for f64 {
+    type Output = Interval;
+    
+    fn add(self, interval: Interval) -> Self::Output {
+        interval.with_displacement(self)
     }
 }
